9d2e848a77a168b2c168ac1be9d488a4dc570eb2
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-07 17:47:58 -0500

    log

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ if (!process.env.DATABASE_URL) {
 	throw new Error("DATABASE_URL is not set");
 }
 
+console.log(process.env.DATABASE_URL)
+
 const db = drizzle(process.env.DATABASE_URL);
 
 const getAllSurveys = async () => {

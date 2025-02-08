81fd32840f740328c69eff40affdd80e6b72b34f
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-08 12:27:13 -0500

    changing public URL for vite

--- frontend/app/treaty.tsx
+++ frontend/app/treaty.tsx
@@ -2,5 +2,5 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "../../index";
 
 export const client = treaty<App>(
-	process.env.RAILWAY_PUBLIC_DOMAIN ?? "localhost:3000",
+	process.env.VITE_RAILWAY_PUBLIC_URL ?? "localhost:3000",
 );
